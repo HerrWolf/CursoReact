cfc7f0772c482410414c3c98c6517e7aa2817cb7
HerrWolf <webmasterp2m@gmail.com>
2020-01-22 13:37:20 -0800

    first commit

---
+++ basicos/src/App.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {  
+
+  // Obtener fecha
+  const fecha = new Date().getFullYear();
+
+  return (
+  <Fragment>
+    
+    <Header
+      titulo='Tienda Virtual'      
+    />
+    
+    <Footer
+        fecha={fecha}
+    />
+    
+  </Fragment>
+    
+  );
+}
+
+export default App;
